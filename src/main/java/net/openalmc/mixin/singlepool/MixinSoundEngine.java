3a3d4e5f48e08b811ade9c6ab0310d645d7a6871
LAGonauta <lagonauta@gmail.com>
2020-08-23 18:33:55 -0300

    Add single source pool for devices with less capacity

--- src/main/java/net/openalmc/mixin/singlepool/MixinSoundEngine.java
+++ src/main/java/net/openalmc/mixin/singlepool/MixinSoundEngine.java
@@ -2,46 +2,53 @@ package net.openalmc.mixin.singlepool;
 
 import net.minecraft.client.sound.SoundEngine;
 import net.minecraft.client.sound.Source;
+import net.openalmc.OpenALMCMod;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(SoundEngine.class)
-public class MixinSoundEngine {
-    @Shadow
-    private SoundEngine.SourceSet streamingSources;
+public abstract class MixinSoundEngine {
+    @Shadow protected abstract int getMonoSourceCount();
 
-    @Shadow
-    private SoundEngine.SourceSet staticSources;
+    @Shadow private SoundEngine.SourceSet staticSources;
 
-    @Inject(
+    @Shadow private SoundEngine.SourceSet streamingSources;
+
+    @Redirect(
             method = "init",
-            at = @At(value = "INVOKE", target = "Lorg/lwjgl/openal/AL;createCapabilities(Lorg/lwjgl/openal/ALCCapabilities;)Lorg/lwjgl/openal/ALCapabilities;", remap = false)
+            at = @At(value = "NEW", target = "Lnet/minecraft/client/sound/SoundEngine$SourceSetImpl;")
     )
-    private void sameSets(CallbackInfo ci) {
-        //this.staticSources = this.streamingSources;
+    private SoundEngine.SourceSetImpl setSourceNumber(int sourceNumber) {
+        return new SoundEngine.SourceSetImpl(getMonoSourceCount());
     }
 
     /**
      * @author LAGonauta
-     * @reason Only one sourcep pool
+     * @reason Only one source pool
      */
-    //@Overwrite
+    @Overwrite
     public String getDebugString() {
-        return String.format("Sounds: %d/%d", this.streamingSources.getSourceCount(), this.streamingSources.getMaxSourceCount());
+        return String.format("Sounds: %d/%d", this.streamingSources.getSourceCount() + this.staticSources.getSourceCount(), this.streamingSources.getMaxSourceCount());
     }
-
 
     /**
      * @author LAGonauta
      * @reason We use only one source pool now
      */
-    //@Overwrite
+    @Overwrite
     public Source createSource(SoundEngine.RunMode mode) {
+        if (mode == SoundEngine.RunMode.STREAMING) {
+            MixinSourceSetAccessor accessor1 = (MixinSourceSetAccessor)this.streamingSources;
+            if (this.streamingSources.getSourceCount() + this.staticSources.getSourceCount() >= this.streamingSources.getMaxSourceCount()) {
+                OpenALMCMod.LOGGER.warn("Dropping static source to make way for streaming source.");
+                MixinSourceSetAccessor accessor = (MixinSourceSetAccessor)this.streamingSources;
+                this.streamingSources.release(accessor.getSourceSet().iterator().next());
+            }
+            return this.staticSources.createSource();
+        }
         return this.streamingSources.createSource();
     }
 }
--- src/main/java/net/openalmc/mixin/singlepool/MixinSourceSet.java
+++
@@ -1,8 +0,0 @@
-package net.openalmc.mixin.singlepool;
-
-import net.minecraft.client.sound.SoundEngine;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(SoundEngine.SourceSetImpl.class)
-public class MixinSourceSet {
-}
